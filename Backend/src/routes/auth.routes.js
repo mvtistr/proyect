313b08d74eeaa8dfc33866ad7ac8783637fb3b11
Roberto Calzadillo <Robertocalzadillo@gmail.com>
2026-03-12 01:30:08 -0400

    Creando las rutas de users usando el modelo vista controlador

---
+++ Backend/src/routes/auth.routes.js
@@ -0,0 +1,14 @@
+const {Router} = require('express')
+
+
+
+
+const {deleteUserController, actualizarController, loginController, registerController} = require('../controllers/auth.controller')
+
+
+const router = Router()
+
+router.post('/users' , registerController)
+router.put('/users/:id', actualizarController)
+router.delete('users/:id' , deleteUserController)
+router.post('/login', loginController)
